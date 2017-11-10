2ab04073ba2eb13ad0cdfeb36fa2a48df9659eed
JloginGit123 <jshri555@gmail.com>
2017-11-10 14:12:01 +0530

    Create OrderService.java

---
+++ OrderService.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+public class OrderService{
+
+public void PlaceOrder(Order order)
+{
+}
+}
